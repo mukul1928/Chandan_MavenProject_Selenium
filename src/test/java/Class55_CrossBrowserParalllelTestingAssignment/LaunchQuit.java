239c42b2367a3e52b3176e62e9b1a42eeeb11e0c
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-27 23:17:47 +0530

    parallel testing

--- src/test/java/Class55_CrossBrowserParalllelTestingAssignment/LaunchQuit.java
+++ src/test/java/Class55_CrossBrowserParalllelTestingAssignment/LaunchQuit.java
@@ -23,14 +23,17 @@ public class LaunchQuit {
 	public void pre_condtition(String nameofbrowser) {
 		if (nameofbrowser.equals("chrome")) {
 			driver = new ChromeDriver();
+			driver.manage().window().maximize();
 		}
 		if (nameofbrowser.equals("firefox")) {
 			driver = new FirefoxDriver();
+			//driver.manage().window().maximize();
 		}
 		if (nameofbrowser.equals("edge")) {
 			driver = new EdgeDriver();
+			//driver.manage().window().maximize();
 		}
-		driver.manage().window().maximize();
+		
 		driver.navigate().to("https://www.google.com");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
