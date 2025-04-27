76e9a003f97fae80040fc4b1bd862500945c77d0
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-27 12:50:24 +0530

    Cross Browser using @Parameters Annotation

---
+++ src/test/java/Chandan_Maven_ProjectAssignments/Selenium/Parameters_Annotation_With_5_Classes.java
@@ -0,0 +1,126 @@
+package Chandan_Maven_ProjectAssignments.Selenium;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Parameters_Annotation_With_5_Classes {
+	WebDriver driver;
+
+	@Test
+	@Parameters("browser")
+	public void google_search_india(String nameofbrowser) {
+
+		if (nameofbrowser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		if (nameofbrowser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		if (nameofbrowser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+
+		driver.navigate().to("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Indian Economy");
+		search.sendKeys(Keys.ENTER);
+	}
+
+	@Test
+	@Parameters("browser")
+	public void google_search_bangalore(String nameofbrowser) {
+		if (nameofbrowser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		if (nameofbrowser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		if (nameofbrowser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Bangalore per capita income");
+		search.sendKeys(Keys.ENTER);
+	}
+
+	@Test
+	@Parameters("browser")
+	public void google_search_Noida(String nameofbrowser) {
+		if (nameofbrowser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		if (nameofbrowser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		if (nameofbrowser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+
+		driver.navigate().to("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Noida best city to visit");
+		search.sendKeys(Keys.ENTER);
+	}
+
+	@Test
+	@Parameters("browser")
+	public void google_search_Ayodhya(String nameofbrowser) {
+		if (nameofbrowser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		if (nameofbrowser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		if (nameofbrowser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+
+		driver.navigate().to("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Ayodhya");
+		search.sendKeys(Keys.ENTER);
+
+	}
+
+	@Test
+	@Parameters("browser")
+	public void google_search_Gorakhpur(String nameofbrowser) {
+		if (nameofbrowser.equals("chrome")) {
+			driver = new ChromeDriver();
+		}
+		if (nameofbrowser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		if (nameofbrowser.equals("edge")) {
+			driver = new EdgeDriver();
+		}
+
+		driver.navigate().to("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("Gorakhpur density");
+		search.sendKeys(Keys.ENTER);
+
+	}
+
+}
