dbecdf4e1d0e557456b087c295e2d924cde0f0bd
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-30 20:14:36 +0530

    dataprovider with 5 sets of datas

--- src/test/java/Chandan_Maven_ProjectAssignments/Selenium/DataProviderMultiDimensionWith5SetOfValues.java
+++ src/test/java/Chandan_Maven_ProjectAssignments/Selenium/DataProviderMultiDimensionWith5SetOfValues.java
@@ -1,6 +1,7 @@
-package class_57_DataProviderTwoDimensional;
+package Chandan_Maven_ProjectAssignments.Selenium;
 
 import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,29 +9,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class DataProviderInTestNG_FB_Login_MultiDimensions {
+public class DataProviderMultiDimensionWith5SetOfValues {
 	WebDriver driver;
 
 	@DataProvider(name = "login_data")
 	public Object[][] method1() {
-		Object data[][] = new Object[2][2];
-		data[0][0] = "rachana@gmail.com";// right un 
-		data[0][1] = "rachana@123";// right pwd {first one is row-> [0], then 2nd one is column ->[1]}
-		data[1][0] = "sree@gmail.com";// right un
-		data[1][1] = "shre@412";// wrong pwd
+		Object[][] data = new Object[6][2];
+		data[0][0] = "rachana@gmail.com";
+		data[0][1] = "rachana@123";
+		data[1][0] = "sree@gmail.com";
+		data[1][1] = "shre@412";
+		data[2][0] = "shiva@gmail.com";
+		data[2][1] = "shiva@4221";
+		data[3][0] = "chandan@gmail.com";
+		data[3][1] = "chandan@321";
+		data[4][0] = "stbymkt@gmail.com";
+		data[4][1] = "Manish!232";
+		data[5][0] = "stbymukul@gmail.com";
+		data[5][1] = "Manis65433";
 		return data;
 	}
 
 	@Test(dataProvider = "login_data")
-	public void Login_to_FB(String un, String Pwd) {
+	public void fb_login(String un, String Pwd) {
 		driver = new ChromeDriver();
 		driver.navigate().to("https://www.facebook.com");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		WebElement email = driver.findElement(By.name("email"));
 		email.sendKeys(un);
-		WebElement pass = driver.findElement(By.id("pass"));
-		pass.sendKeys(Pwd);
+		WebElement pwd = driver.findElement(By.id("pass"));
+		pwd.sendKeys(Pwd);
 		WebElement login = driver.findElement(By.name("login"));
 		login.click();
 		System.out.println(un + Pwd);
