495cc93c96d3066bbce0d03376919872299f0be8
Chandan Pandey <chandanpandey1928@gmail.com>
2025-04-30 21:14:16 +0530

    assertion in TestNG

---
+++ src/test/java/class_58_Assertion_In_TestNG/Amazon_Login_With_Assertion_TestNG.java
@@ -0,0 +1,5 @@
+package class_58_Assertion_In_TestNG;
+
+public class Amazon_Login_With_Assertion_TestNG {
+
+}
---
+++ src/test/java/class_58_Assertion_In_TestNG/HowToPass_TestCases.java
@@ -0,0 +1,21 @@
+package class_58_Assertion_In_TestNG;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class HowToPass_TestCases {
+	@Test
+	public void testcase1() {
+		int a = 100;
+		//Assert.assertTrue(true);// Assert is class here and will be only used in @Test in TestNG
+		//Assert.assertTrue(false);// if I want my test case to be failed then I can use this
+		//Assert.assertFalse(false);
+		//Assert.assertFalse(a<99);
+		//Assert.assertEquals(true, true);
+		//Assert.assertEquals(true, false,"let's retry");
+		//Assert.assertEquals("Chandan", "chandan","Sorry, two strings are not equals");
+		//Assert.assertEquals('a', 'a');
+		
+	}
+
+}
