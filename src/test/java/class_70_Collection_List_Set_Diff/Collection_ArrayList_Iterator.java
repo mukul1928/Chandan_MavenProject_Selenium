908af8f3bf9a94cb2e6de874a4962ed8f152b4a9
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-05 11:47:52 +0530

    Collection Array list Iterator

--- src/test/java/class_70_Collection_List_Set_Diff/Collection_ArrayList_Iterator.java
+++ src/test/java/class_70_Collection_List_Set_Diff/Collection_ArrayList_Iterator.java
@@ -15,9 +15,9 @@ public class Collection_ArrayList_Iterator {
 		System.out.println(l1);// List follow Indexing
 
 		Iterator i2 = l1.iterator();
-		while(i2.hasNext()) 
+		while(i2.hasNext()) //hasNext is going to check whether my return true iteration has more elements in it
 		{
-			System.out.println();
+			System.out.println(i2.next());
 		}
 	}
 
